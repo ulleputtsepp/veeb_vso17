cb031110f49ee7144a46f97ef6fd9030c9c1d642
ulleputtsepp <ulle.puttsepp@khk.ee>
2017-12-08 09:38:03 +0200

    Projekti nimetus kirjeldatud kommentaaride kasutamine

--- index.php
+++ index.php
@@ -12,4 +12,10 @@
  * mitme-
  * realine kommentaar
  */
+//stringide väljastamine
+print "See on tavaline tekst";
+print 'See on ka tavaline tekst';
+// echo väljastamiskaäsuga
+echo "See on tavaline tekst jutumärkides";
+echo 'See on tavaline tekst ülekomades';
 ?>
