ec7ecb2568bf6da581a9e2645291b8c5704825ca
ulleputtsepp <ulle.puttsepp@khk.ee>
2017-12-08 09:27:51 +0200

    Projekti nimetus kirjeldatud kommentaaride kasutamine

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ulle
+ * Date: 08-Dec-17
+ * Time: 09:05
+ */
+# üherealine see on kommentaar - ei kasuta eriti
+// üherealine kommentaar
+/*
+ * mitme-
+ * realine kommentaar
+ */
+?>
