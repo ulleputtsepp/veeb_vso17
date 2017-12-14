383feeda795e8dc0400f5ff329866afd9fcb5eec
ulleputtsepp <ulle.puttsepp@khk.ee>
2017-12-14 15:05:54 +0200

    lihtne muutuja

--- mina.php
+++ mina.php
@@ -6,8 +6,10 @@
  * Time: 12:21
  */
 $nimi="Ulle";
-    echo "Minu nimi on:"." "$nimi.;
-?>
- /** konstandi määramine, väärtuse andmine, echo-s $ pole vaja konstandis*/ 
+    echo "Minu nimi on:"." ".$nimi.'<br />';
+
+// konstandi määramine, väärtuse andmine, echo-s $-märki pole vaja konstandis
  define('MINU_NIMI','Ulle');
  echo 'Minu nimi on  '.MINU_NIMI.'<br />';
+
+?>
