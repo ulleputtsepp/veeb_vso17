3722db57ea1adaf67dcf0b83c9813d5ab3e01b0d
ulleputtsepp <ulle.puttsepp@khk.ee>
2018-01-25 11:03:32 +0200

    koostatud konstruktor

--- tekst.php
+++ tekst.php
@@ -10,7 +10,18 @@ class tekst
 { // klassi algus
     // klassi omadused (väljad, fields) - muutujad, mida defineerime
     // klassi muutuja var mingi algväärtusega, kirjeldus peaks olema võimalikult tühi, et lubaks varieeruvust
-    var $sone = ''; // teksti kirjeldavad sõnad
+    var $sone = '';
+
+    /**
+     * tekst constructor.
+     * @param string $sone
+     */
+    public function __construct($sone)
+    {
+        $this->maaraTekst($sone);
+        // $this->sone = $sone;
+    }
+    // teksti kirjeldavad sõnad
     // klassi tegevused meetoditena kirja, kuidas sooritada üksiktegevused, väga detailselt
     // millise sisuga teskti tahan kirjutada
     // käima: maaraTekst('Tere maailm!');
@@ -21,4 +32,5 @@ class tekst
     function prindiTekst(){
         echo $this->sone.'<br />'; // esialgu puhas leht; teen uue tavalise PHP faili tesktitest.php
     }
+
 } // klassi lõpp
