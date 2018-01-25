397fca2c675351602b12ba2c8c6c0c24bc44813c
ulleputtsepp <ulle.puttsepp@khk.ee>
2018-01-25 11:08:19 +0200

    veel üks tekst

--- tekstitest.php
+++ tekstitest.php
@@ -8,12 +8,19 @@
 require _once 'tekst.php';
 
 // loome teksti objekt
-$minuTekst = new tekst(); //teksti töötluse element, see tuli klassifailist
+// $minuTekst = new tekst(); //teksti töötluse element, see tuli klassifailist
 // määrame konkretse sisu
-$minuTekst->maaraTekst('Tere maailm!');
+$minuTekst-> new tekst('Teme maailm!');// välja, sest on juba konstruktoris: maaraTekst('Tere maailm!');
 // vaatame objekti sisse
 echo '<pre>';
 print_r($minuTekst);
 echo '</pre>';
 // prindime välja
 $minuTekst->prindiTekst();
+
+// loome uue teksti
+$mustTekst = new tekst('Must Tekst');
+echo '<pre>';
+print_r($mustTekst);
+echo '</pre>';
+$mustTekst->prindiTekst();
