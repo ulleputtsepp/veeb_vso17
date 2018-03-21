7429d8216b4402d18877fab81e31d3057341358d
ulleputtsepp <ulle.puttsepp@khk.ee>
2018-03-21 16:47:46 +0200

    numbrid värvilistena for-tsükkel, operaatorid ++ ja --, while ja do-while koos break continue

--- numbrid.php
+++ numbrid.php
@@ -4,14 +4,50 @@
  * User: Ulle
  * Date: 14-Dec-17
  * Time: 12:29 */
-for ($number=1; $number<=10; $number++){
-    echo $number.'<br />';
-    if($number % 2 != 0) {
+// numbrid väljastatud värviliselt
+for ($number=1; $number <=10; $number++){
+    //echo $number.'<br />';
+    if($number % 2 == 0) {
         $varv = 'blue';
     }
         else{
             $varv = 'red';
         }
-        echo '<font color =" '.$varv.'">'.$number.'</font><br />';
+        echo '<font color ="'.$varv.'">'.$number.'</font><br />';
     }
+//operaatorid ++ ja --
+    $a=5; $b=2;
+    $c=$a++;
+    $d=$b--;
+    echo 'a= '.$a.'<br />'; //a=6
+    echo 'b= '.$b.'<br />'; //b=1
+    echo 'c= '.$c.'<br />'; //c=5
+    echo 'd= '.$d.'<br />'; //d=2
+
+    $a=5; $b=2;
+    $c = ++$a;
+    $d = --$b;
+    echo 'a= '.$a.'<br />'; //a=6
+    echo 'b= '.$b.'<br />'; //b=1
+    echo 'c= '.$c.'<br />'; //c=6
+    echo 'd= '.$d.'<br />'; //d=1
+
+// while ja do-while tsüklid koos break ja continuega
+$kord=1;
+while($kord <= 5){
+    if($kord == 3){
+        break;
+    }
+    echo '<i>'.$kord++.'</i><br />'; //12 italic
+}
+//
+$kord=1;
+do {
+    if($kord == 3){
+        continue;
+    }
+    echo '<b>'.$kord.'</b><br />';
+} while(++$kord <= 5); //1245 bold
+
+
 ?>
